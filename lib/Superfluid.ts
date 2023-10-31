d48e04fe41f9434b51cd127db23ccbecd2a4ebf0
Trymbak Mahant <trymbak0102mahant@gmail.com>
2023-11-01 01:41:15 +0530

    polybase added

--- lib/Superfluid.ts
+++ lib/Superfluid.ts
@@ -3,32 +3,34 @@ import { ethers } from "ethers";
 
 export async function createNewFlow(recipient: string, flowRate: string) {
   try {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    await provider.send("eth_requestAccounts", []);
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
 
-    const signer = await provider.getSigner();
-    const sf = await Framework.create({
-      chainId: 80001,
-      provider: provider,
-    });
-    const superSigner = sf.createSigner({ signer: signer });
-    const daix = await sf.loadSuperToken("fDAIx");
-
-    //console.log(daix);
-
-    try {
-      const createFlowOperation = daix.createFlow({
-        sender: await superSigner.getAddress(),
-        receiver: recipient,
-        flowRate: flowRate,
-        // userData?: string
+      const signer = await provider.getSigner();
+      const sf = await Framework.create({
+        chainId: 80001,
+        provider: provider,
       });
+      const superSigner = sf.createSigner({ signer: signer });
+      const daix = await sf.loadSuperToken("fDAIx");
 
-      const result = await createFlowOperation.exec(superSigner);
-      //console.log(result);
-      return result;
-    } catch (error) {
-      return error;
+      //console.log(daix);
+
+      try {
+        const createFlowOperation = daix.createFlow({
+          sender: await superSigner.getAddress(),
+          receiver: recipient,
+          flowRate: flowRate,
+          // userData?: string
+        });
+
+        const result = await createFlowOperation.exec(superSigner);
+        //console.log(result);
+        return result;
+      } catch (error) {
+        return error;
+      }
     }
   } catch (e) {
     console.log(e);
@@ -36,27 +38,29 @@ export async function createNewFlow(recipient: string, flowRate: string) {
 }
 
 export async function GetFlow(recipient: string) {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  await provider.send("eth_requestAccounts", []);
-
-  const sf = await Framework.create({
-    chainId: 80001,
-    provider: provider,
-  });
-
-  const daix = await sf.loadSuperToken("fDAIx");
-  const signer = await provider.getSigner();
-  const superSigner = sf.createSigner({ signer: signer });
-  // console.log(daix);
-
   try {
-    const flowdata = await daix.getFlow({
-      sender: await superSigner.getAddress(),
-      receiver: recipient,
-      providerOrSigner: provider,
-    });
-    //console.log(flowdata);
-    return flowdata;
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+
+      const sf = await Framework.create({
+        chainId: 80001,
+        provider: provider,
+      });
+
+      const daix = await sf.loadSuperToken("fDAIx");
+      const signer = await provider.getSigner();
+      const superSigner = sf.createSigner({ signer: signer });
+      // console.log(daix);
+
+      const flowdata = await daix.getFlow({
+        sender: await superSigner.getAddress(),
+        receiver: recipient,
+        providerOrSigner: provider,
+      });
+      //console.log(flowdata);
+      return flowdata;
+    }
   } catch (error) {
     return error;
   }
@@ -64,34 +68,36 @@ export async function GetFlow(recipient: string) {
 
 // function for deleteing  existing flow
 export async function deleteExistingFlow(recipient: string) {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  await provider.send("eth_requestAccounts", []);
-
-  const signer = provider.getSigner();
-
-  const chainId = await window.ethereum.request({ method: "eth_chainId" });
-  const sf = await Framework.create({
-    chainId: Number(chainId),
-    provider: provider,
-  });
-
-  const superSigner = sf.createSigner({ signer: signer });
-
-  // console.log(signer);
-  // console.log(await superSigner.getAddress());
-  const daix = await sf.loadSuperToken("fDAIx");
-
-  // console.log(daix);
-
   try {
-    const deleteFlowOperation = daix.deleteFlow({
-      sender: await signer.getAddress(),
-      receiver: recipient,
-      // userData?: string
-    });
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
 
-    const result = await deleteFlowOperation.exec(superSigner);
-    return result;
+      const signer = provider.getSigner();
+
+      const chainId = await window.ethereum.request({ method: "eth_chainId" });
+      const sf = await Framework.create({
+        chainId: Number(chainId),
+        provider: provider,
+      });
+
+      const superSigner = sf.createSigner({ signer: signer });
+
+      // console.log(signer);
+      // console.log(await superSigner.getAddress());
+      const daix = await sf.loadSuperToken("fDAIx");
+
+      // console.log(daix);
+
+      const deleteFlowOperation = daix.deleteFlow({
+        sender: await signer.getAddress(),
+        receiver: recipient,
+        // userData?: string
+      });
+
+      const result = await deleteFlowOperation.exec(superSigner);
+      return result;
+    }
     // console.log(result);
   } catch (error) {
     return error;
@@ -99,30 +105,32 @@ export async function deleteExistingFlow(recipient: string) {
 }
 
 export async function UpdateFlow(recipient: string, flowRate: string) {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  await provider.send("eth_requestAccounts", []);
-
-  const signer = await provider.getSigner();
-  const sf = await Framework.create({
-    chainId: 80001,
-    provider: provider,
-  });
-  const superSigner = sf.createSigner({ signer: signer });
-  const daix = await sf.loadSuperToken("fDAIx");
-
-  //  console.log(daix);
-
   try {
-    const updateFlowOperation = daix.updateFlow({
-      sender: await superSigner.getAddress(),
-      receiver: recipient,
-      flowRate: flowRate,
-      // userData?: string
-    });
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
 
-    const result = await updateFlowOperation.exec(superSigner);
-    //  console.log(result);
-    return result;
+      const signer = await provider.getSigner();
+      const sf = await Framework.create({
+        chainId: 80001,
+        provider: provider,
+      });
+      const superSigner = sf.createSigner({ signer: signer });
+      const daix = await sf.loadSuperToken("fDAIx");
+
+      //  console.log(daix);
+
+      const updateFlowOperation = daix.updateFlow({
+        sender: await superSigner.getAddress(),
+        receiver: recipient,
+        flowRate: flowRate,
+        // userData?: string
+      });
+
+      const result = await updateFlowOperation.exec(superSigner);
+      //  console.log(result);
+      return result;
+    }
   } catch (error) {
     return error;
   }
